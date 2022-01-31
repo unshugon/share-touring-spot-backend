00a559d9b7b120792b6d85c1ad54325245009d75
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-01-31 13:31:39 +0900

    modify hash filename

--- posts/spots/models.py
+++ posts/spots/models.py
@@ -1,10 +1,21 @@
+from datetime import datetime
+import hashlib
 from django.db import models
 from django.conf import settings
 
 
+def _post_image_upload_to(instance, filename):
+    current_time = datetime.now()
+    pre_hash_name = "%s%s%s" % (instance.title, filename, current_time)
+    extension = str(filename).split(".")[-1]
+    hs_filename = "%s.%s" % (hashlib.md5(pre_hash_name.encode()).hexdigest(), extension)
+    saved_path = "images/"
+    return "%s%s" % (saved_path, hs_filename)
+
+
 class Post(models.Model):
     title = models.CharField("タイトル", max_length=50)
-    image = models.ImageField(upload_to="images", verbose_name="イメージ画像")
+    image = models.ImageField(upload_to=_post_image_upload_to, verbose_name="イメージ画像")
     content = models.TextField("本文", max_length=500)
     created_at = models.DateTimeField("作成日", auto_now_add=True)
     created_by = models.ForeignKey(
