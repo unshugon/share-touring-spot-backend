3c985a60f913feb855c991fcee7e32ec687820db
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-02-02 00:48:19 +0900

    fix boto3

--- share_touring_spot/settings.py
+++ share_touring_spot/settings.py
@@ -173,7 +173,7 @@ else:
     AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
     AWS_STORAGE_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
 
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3BotoStorage"
     S3_URL = "http://%s.s3.amazonaws.com/" % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = S3_URL
 
