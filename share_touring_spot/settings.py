b96555920ca7c538bdf001eb9d943fb05125d216
unshugon <unshugohn.motorcycle.lgm@gmail.com>
2022-02-07 23:33:50 +0900

    fix database default engine

--- share_touring_spot/settings.py
+++ share_touring_spot/settings.py
@@ -185,6 +185,7 @@ else:
 
     db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
     DATABASES = {"default": dj_database_url.config()}
+    DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
